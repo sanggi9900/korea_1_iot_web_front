1699cd7fcee4c540ca6756cb5292aff01aaedf8b
tkdrl9900 <tkdrl9900@gmail.com>
2024-08-08 17:42:30 +0900

    20240808

---
+++ c_javascript/j_배열_함수_객체/practice01.js
@@ -0,0 +1,46 @@
+// practice01.js
+
+
+// 1. 주제선정
+// 점심목록
+
+// 1) 추가 점심 추가 하기
+// 2) 조회 현재 점심 목록 보기
+// 3) 수정 특정 점심 수정하기
+// 4) 삭제 특정 점심 삭제하기
+
+// +) 먹은 점심메뉴 계산하기
+// +) 전체 점심메뉴 제거하기
+
+//? 점심 객체 예시
+let hansic= {
+ id: 1,
+ price: 8.5,
+CoName: 'Bab&Bab'
+}
+
+ let mandu = {
+  id: 2,
+  price: 6.8,
+  CoName: 'South Country hand'
+ }
+
+let garbi = {
+  id: 3,
+  price: 7.5,
+  CoName: 'Hangeun Galbi'
+}
+
+let Udon = {
+  id: 4,
+  price: 8.0,
+  CoName: 'DayUdon'
+}
+
+let gimbab = {
+  id: 5,
+  price: 7.0,
+  CoName: 'GrandMoMhand '
+}
+
+let hangry = [];
