c295e93196db8dcf7999f8cdf524acb134679d86
tkdrl9900 <tkdrl9900@gmail.com>
2024-08-19 10:29:04 +0900

    20240819

---
+++ d_typescript/b_환경설정/a_settings.ts
@@ -0,0 +1,31 @@
+// a_settings.ts
+
+/*
+# 타입스크립트 환경설정
+
+! 1.Node.js 설치
+
+- Node.js 설치 확인 명령어
+: cmd (명령 프롬프트 창)
+  node -v
+  npm -v
+
+  ?  npm 기본 명령어
+  
+  1. npm init
+    : 새로운 Node.js 프로젝트를 시작
+*/
+
+
+{
+  "name": "d_typescript",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": ""
+}
