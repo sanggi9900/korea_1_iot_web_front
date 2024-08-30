741ed780988c91ee0063b76e49d7cb0f6b259123
tkdrl9900 <tkdrl9900@gmail.com>
2024-08-30 17:48:14 +0900

    20240830

--- e_react/src/pages/b_hooks/index.tsx
+++ e_react/src/pages/b_hooks/index.tsx
@@ -16,16 +16,31 @@ import TodoAppLocalStorage from "./z_Todo/TodoAppLocalStorage";
 import UseMemo01 from "./d_Use/UseMemo01";
 import UseMemo02 from "./d_Use/UseMemo02";
 import UseCallback from "./d_Use/UseCallback";
+import UseReducer01 from "./e_UseReducer/UseReducer01";
+import UseReducer02 from "./e_UseReducer/UseReducer02";
+import Custom01 from "./f_CustomHook/Custom01";
+import Custom02 from "./f_CustomHook/Custom02";
+
+import { useLocation } from "react-router-dom";
 
 // 전체 파일 정렬
 // ctrl + a: 전체 선택
 // ctrl + k + f: 포맷터 사용
 function Index() {
+  const location = useLocation();
+  const { username } = location.state || {};
+
   return (
     <div>
       <h1 style={{ backgroundColor: "black", color: "white" }}>
         리액트 Hooks
       </h1>
+      
+      {username}
+  
+      <h2>리액트 Hooks - 커스텀 훅</h2>
+      <Custom01 />
+      <Custom02 />
 
       <h2>리액트 Hooks - useState</h2>
       <UseState01 />
@@ -59,6 +74,11 @@ function Index() {
 
       <UseCallback />
     
+      <hr />
+
+      <h2>리액트 Hooks - useReducer</h2>
+      <UseReducer01 />
+      <UseReducer02 />
     </div>
   );
 }
